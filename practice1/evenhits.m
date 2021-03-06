01bac0dde713fbbeb1a5de7c10844a633ce86f91
Solviana <solviana93@gmail.com>
2021-03-06 08:03:52 +0100

    1st problem cleanup

--- practice1/evenhits.m
+++ practice1/evenhits.m
@@ -1,11 +1,7 @@
-% NSHOT=10
-% PHIT=0.1
-% NTEST = 10000
-% anal=getprobabilityofevenhit_analytical(NSHOT, PHIT)
-% montec=getprobabilityofevenhit_monteCarlo(NSHOT,PHIT,NTEST)
 testProbabilityOfEvenHit(21,5000)
+
 function testProbabilityOfEvenHit(nShot, nSim)
-    pHit = 0:0.01:1;
+    pHit = 0:0.01:1; 
     pEvenanalytical = arrayfun(@(x) getprobabilityofevenhit_analytical(nShot, x), pHit);
     pEvenMonteCarlo = arrayfun(@(x) getprobabilityofevenhit_montecarlo(nShot, x, nSim), pHit);
     plot(pHit, pEvenanalytical, pHit, pEvenMonteCarlo);
@@ -16,9 +12,8 @@ end
 
 
 function pEven = getprobabilityofevenhit_analytical (nShot, pHit)
-    % brute force
-    possibleHits = 0:2:nShot;
     % sum up Ps of each even hitcount
+    possibleHits = 0:2:nShot;
     pEven = sum(...
         arrayfun(@(x) getprobabilityofhits_analytical(nShot,pHit,x), possibleHits));
 end
@@ -29,6 +24,8 @@ function p = getprobabilityofhits_analytical(nShot, pHit, nHit)
 end
     
 function pEven = getprobabilityofevenhit_montecarlo (nShot, pHit, nSim)
+    % columns contain individual games, rows contain the shots
+    % 1 is a hit 0 is a miss
     bHits = rand([nSim, nShot]) < pHit;
     bEvenGames = zeros([1,nSim]);
     for i = 1:nSim
